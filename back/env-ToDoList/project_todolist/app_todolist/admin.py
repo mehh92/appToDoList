5089369c009c3946cf467d4b088eafbe57cbd0ea
Mehdi Chiche <mehdi@MacBook-Mehdi.local>
2024-06-13 17:42:54 +0200

    Fix authentification via token

--- back/env-ToDoList/project_todolist/app_todolist/admin.py
+++ back/env-ToDoList/project_todolist/app_todolist/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Utilisateur, Todolist, Tache, Suggestion
+# from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Utilisateur, UserAdmin)
+# admin.site.register(Todolist)
+# admin.site.register(Tache)
+# admin.site.register(Suggestion)
--- back/env-ToDoList/project_todolist/project_todolist/settings.py
+++ back/env-ToDoList/project_todolist/project_todolist/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        #'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAuthenticated',
